cecd1780d25c36775c717dacbb8416cbc3461aae
driversline <161175951+driversline@users.noreply.github.com>
2025-04-07 16:39:01 +0300

    home.nix

---
+++ template/home.nix
@@ -0,0 +1,33 @@
+{ 
+config, 
+pkgs, 
+... 
+}: 
+{
+home = {
+stateVersion = "24.11";
+username = "user";
+homeDirectory = "/home/user";
+pointerCursor =
+let
+  getFrom = url: hash: name: {
+      gtk.enable = false;
+      x11.enable = true;
+      name = name;
+      size = 18;
+      package =
+        pkgs.runCommand "moveUp" {} ''
+          mkdir -p $out/share/icons
+          ln -s ${pkgs.fetchzip {
+            url = url;
+            sha256 = hash;
+          }} $out/share/icons/${name}
+      '';
+    };
+in
+  getFrom
+    "https://github.com/Ваш-Юзернейм/Название-Репозитория/raw/main/Директория-В-Репозитории/Название-Архива.tar.gz"
+    "Полученный-Хеш (nix-prefetch-url --unpack)"
+    "Название-Папки-Курсора (без .tar.gz)";
+};
+}
